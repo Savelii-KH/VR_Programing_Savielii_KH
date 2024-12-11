a107d9070b9fa2e6c1e65437881b87fbb414d787
Savielii <s.i.khizhnikov@student.khai.edu>
2024-12-11 10:04:36 +0200

    Texture and Noise generator
    In this commit, I added a texture generator and now we can see the noise in two modes: Noise (from black to white) and from a custom color map created with a height map

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour 
+{
+    public enum DrawMode { NoiseMap, ColourMap }
+    public DrawMode drawMode;
+
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+
+    [Range(0.01f, 5)]
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+
+    public TerrainType[] regions;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float curentHeight = noiseMap[x, y];
+
+                for (int i = 0; i < regions.Length; i++)
+                {
+                   if (curentHeight <= regions[i].height)
+                   {
+                        colorMap[y * mapWidth + x] = regions[i].color;
+                        break;
+                   }
+                }
+            }
+        }
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode == DrawMode.NoiseMap) 
+        { 
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap)); 
+        }
+        else if (drawMode == DrawMode.ColourMap) 
+        {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colorMap, mapWidth, mapHeight));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (mapHeight < 1) { mapHeight = 1; }
+        if (mapWidth < 1) { mapWidth = 1; }
+        if (noiseScale < 0.0001f) { noiseScale = 0.0001f; }
+        if (octaves < 1) { octaves = 1; }
+    }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+}
